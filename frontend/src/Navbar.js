05f521b4eb840ade10f47211114b6f83843c60b5
patrikkuric <kuricpatrik1@gmail.com>
2024-01-21 23:38:39 +0100

    frontend final fix

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -1,52 +1,60 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import useTokenStore from "./token";
-import React from "react";
+import React, { useState } from "react";
 
 export default function Navbar() {
-    const { username, role  } = useTokenStore();
+    const { username, role } = useTokenStore();
+    const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+    const handleNavbarLinkClick = () => {
+        setMobileMenuOpen(false);
+    };
+
+    const handleToggleMenu = () => {
+        setMobileMenuOpen(!isMobileMenuOpen);
+    };
 
     return (
-        <nav className="navbar sticky-top fixed-top navbar-expand-lg navbar-dark bg-dark navbarOne">
+        <nav className={`navbar sticky-top fixed-top navbar-expand-lg navbar-dark bg-dark navbarOne ${isMobileMenuOpen ? 'navbar-open' : ''}`}>
             <div className="mainLogo">
                 <Link to="/" className="navbar-brand" href="#">Quasar</Link>
             </div>
-            <button className="navbar-toggler"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation">
+            <button
+                className="navbar-toggler"
+                type="button"
+                onClick={handleToggleMenu}
+            >
                 <span className="navbar-toggler-icon"></span>
             </button>
 
-            <div className="collapse navbar-collapse containers" id="navbarSupportedContent">
+            <div className={`collapse navbar-collapse containers ${isMobileMenuOpen ? 'show' : ''}`} id="navbarSupportedContent">
                 <ul className="navbar-nav ms-auto me-3">
-                    {/*
-                    <li className="nav-item nav-item-mainNavbar tech">
-                        <Link to="/tech" className="nav-link" href="#">Tech</Link>
-                    </li>
-                    */}
                     <li className="nav-item nav-item-mainNavbar about">
-                        <Link to="/" className="nav-link" href="#">Home</Link>
+                        <Link to="/" className="nav-link" onClick={handleNavbarLinkClick}>
+                            Home
+                        </Link>
                     </li>
                     <li className="nav-item nav-item-mainNavbar games">
-                        <Link to="/games" className="nav-link" href="#">Games</Link>
+                        <Link to="/games" className="nav-link" onClick={handleNavbarLinkClick}>
+                            Games
+                        </Link>
                     </li>
                     {role === "Administrator" && (
                         <li className="nav-item nav-item-mainNavbar admin">
-                            <Link to="/admin" className="nav-link" href="#">Admin</Link>
+                            <Link to="/admin" className="nav-link" onClick={handleNavbarLinkClick}>
+                                Admin
+                            </Link>
                         </li>
                     )}
                     {username ? (
                         <li className="nav-item nav-item-mainNavbar account">
-                            <Link to="/account" className="nav-link" href="#">
+                            <Link to="/account" className="nav-link" onClick={handleNavbarLinkClick}>
                                 {username}
                             </Link>
                         </li>
                     ) : (
                         <li className="nav-item nav-item-mainNavbar signup">
-                            <Link to="/sign-up" className="nav-link" href="#">
+                            <Link to="/sign-up" className="nav-link" onClick={handleNavbarLinkClick}>
                                 SIGN UP
                             </Link>
                         </li>
@@ -56,4 +64,3 @@ export default function Navbar() {
         </nav>
     );
 }
-
